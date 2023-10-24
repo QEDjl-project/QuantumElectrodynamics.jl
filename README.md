5c919a47a3a489e6136537c5be43866564857f23
Simeon Ehrig <s.ehrig@hzdr.de>
2023-10-24 14:58:52 +0200

    add Github Action to build and deploy documentation (#24)

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # QED
 
-[![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://szabo137.github.io/QED.jl/stable/)
-[![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://szabo137.github.io/QED.jl/dev/)
+[![Doc Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://qedjl-project.github.io/QED.jl/main)
+[![Doc Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://qedjl-project.github.io/QED.jl/dev)
 [![Code Style: Blue](https://img.shields.io/badge/code%20style-blue-4495d1.svg)](https://github.com/invenia/BlueStyle)
 
 ## Installation
--- docs/make.jl
+++ docs/make.jl
@@ -6,15 +6,15 @@ DocMeta.setdocmeta!(QED, :DocTestSetup, :(using QED); recursive=true)
 makedocs(;
     modules=[QED],
     authors="Uwe Hernandez Acosta <u.hernandez@hzdr.de>, Simeon Ehrig, Klaus Steiniger, Tom Jungnickel, Anton Reinhard",
-    repo="https://www.github.com/szabo137/QED.jl/blob/{commit}{path}#{line}",
+    repo=Documenter.Remotes.GitHub("QEDjl-project", "QED.jl"),
     sitename="QED.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://szabo137.github.io/QED.jl",
-        edit_link="main",
+        canonical="https://qedjl-project.github.io/QED.jl/",
+        edit_link="dev",
         assets=String[],
     ),
     pages=["Home" => "index.md", "Automatic Testing" => "ci.md"],
 )
 
-deploydocs(; repo="www.github.com/szabo137/QED.jl", devbranch="main")
+deploydocs(; repo="github.com/QEDjl-project/QED.jl.git", push_preview=false)
